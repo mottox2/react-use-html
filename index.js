1a05f6a13ece5bcf833cd0f22d7725c51e798012
mottox2 <y.mottox2@gmail.com>
2021-10-17 21:53:37 +0900

    Init

---
+++ index.js
@@ -0,0 +1,19 @@
+const { useMemo } = require("react");
+const parse = require("html-react-parser");
+
+exports.useHtml = (html, components) => {
+  const elements = useMemo(() => {
+    const options = {
+      replace: (node) => {
+        if (!(node instanceof Element) || typeof node === "string") return node;
+        const props = attributesToProps(node.attribs);
+        const children = domToReact(node.children, options);
+        const render = components[node.name];
+        if (render) return render({ name: node.name, props, children });
+        return node;
+      },
+    };
+    return parse(html, options);
+  }, [html, components]);
+  return elements;
+};
